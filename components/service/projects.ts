2ef7144fa8d354b5c7a5c21473d2c75a7537a3ff
Shalini Vithanage <shalinirvithanage@gmail.com>
2026-01-23 16:06:15 +0530

    update

--- components/service/projects.ts
+++ components/service/projects.ts
@@ -10,16 +10,16 @@ export interface Project {
 }
 
 export const projects: Project[] = [
-    {
+     {
         id: 1,
-        title: "Shinez - Cabinet Making Web Application",
+        title: "Dream Coach Lines - Tourism Web Application",
         description:
-            "Australia-Based Cabinet Making Web Application using Wordpress and Elementor to showcase custom cabinetry services, portfolio, and client testimonials.",
-        technologies: ["Wordpress", "Elementor", "HTML", "CSS"],
+            "Canada-Based Tourism Web Application with payment gateway integration, email notifications with QR codes and a QR Code Scanner for ticket verification.",
+        technologies: ["HTML", "CSS", "JAVASCRIPT", "FIREBASE"],
         type: "web",
-        image: "/images/Cabinet.JPG?height=300&width=400",
-        github: "https://shinez.com.au/",
-        live: "https://shinez.com.au/",
+        image: "/images/dream.JPG?height=300&width=400",
+        github: "https://github.com/shaliniPerer/Dream_Coach_Lines",
+        live: "https://www.dreamcoachlines.com//",
     },
     {
         id: 2,
@@ -32,51 +32,19 @@ export const projects: Project[] = [
         github: "https://rich.thewavedigital.com.lk/",
         live: "https://rich.thewavedigital.com.lk/",
     },
-    {
-        id: 3,
-        title: "Prime Cleanings - Cleaning Services Web Application",
+      {
+         id: 3,
+        title: "Mango WebSite",
         description:
-            "Australia-Based Cleaning Services Web Application using Wordpress and Elementor to showcase residential and commercial cleaning services.",
-        technologies: ["Wordpress", "Elementor", "HTML", "CSS"],
+            "One page website for Mango - A fictional fruit selling company.",
+        technologies: ["HTML", "CSS", "JavaScript"],
         type: "web",
-        image: "/images/cleaning.JPG?height=300&width=400",
-        github: "https://primecleanings.com.au/",
-        live: "https://primecleanings.com.au/",
+        image: "/images/mango.JPG?height=300&width=400",
+        github: "https://github.com/shaliniPerer/website",
+        live: "https://website-svrh.vercel.app/",
     },
-     {
-        id: 4,
-        title: "Australia Eco Spark - Cleaning Services Web Application",
-        description:
-            "Transforming everyday spaces into sparkling environments.",
-        technologies: ["Wordpress", "Elementor", "HTML", "CSS"],
-        type: "web",
-        image: "/images/spark.JPG?height=300&width=400",
-        github: "https://ecosparkcco.com.au/",
-        live: "https://ecosparkcco.com.au/",
-
-        // id: 4,
-        // title: "Hostel Management Mobile Application",
-        // description:
-        //     "Built a mobile application using Flutter and Firebase for hostel room bookings, request submissions, QR code attendence tracking, and in- app messaging for students.",
-        // technologies: ["Flutter", "Dart", "Firebase", "Cloud Firestore"],
-        // type: "mobile",
-        // image: "/images/hostel.JPG?height=300&width=400",
-        // github: "https://github.com/shaliniPerer/Hostel_Management_System",
-        // live: "https://github.com/shaliniPerer/Hostel_Management_System",
-    },
-    {
-        id: 5,
-        title: "Dream Coach Lines - Tourism Web Application",
-        description:
-            "Canada-Based Tourism Web Application with payment gateway integration, email notifications with QR codes and a QR Code Scanner for ticket verification.",
-        technologies: ["HTML", "CSS", "JAVASCRIPT", "FIREBASE"],
-        type: "web",
-        image: "/images/dream.JPG?height=300&width=400",
-        github: "https://dreamcoachlines.com/",
-        live: "https://github.com/shaliniPerer/Dream_Coach_Lines",
-    },
-    {
-         id: 6,
+   {
+         id: 4,
         title: "InvesterPro - Investment Management Web Application",
         description:
             "Management of investment portfolios, tracking stocks, bonds, and mutual funds and providing real-time market data.",
@@ -86,5 +54,29 @@ export const projects: Project[] = [
         github: "https://github.com/shaliniPerer/investment",
         live: "https://investment-iota-gules.vercel.app/",
     },
+    
+    {
+         id: 5,
+        title: "Auto Grader",
+        description:
+            "AI Assignment Corrector.Uses NLP techniques to evaluate and grade student assignments automatically, providing feedback on grammar, coherence, and content quality.",
+        technologies: ["Next", "Python", "Node.js"],
+        type: "web",
+        image: "/images/grader.JPG?height=300&width=400",
+        github: "https://github.com/dewmindi/autoGrader",
+        live: "https://github.com/dewmindi/autoGrader",
+    },
+    {
+        id: 5,
+        title: "Tourism Web Application",
+        description:
+            "Tourism Web Application For Booking Sri Lankan Tourist Destinations , And Enhance The User Experience.",
+        technologies: ["HTML", "CSS", "JAVASCRIPT", "FIREBASE"],
+        type: "web",
+        image: "/images/tourist.JPG?height=300&width=400",
+        github: "https://github.com/shaliniPerer/MS-Travel",
+        live: "https://github.com/shaliniPerer/MS-Travel",
+    },
+    
     
 ]
